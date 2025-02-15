7ab225252f9d158daced90eb4213743fa8731820
Alex Bilozor <bilazor@gmail.com>
2025-02-14 16:28:14 -0800

    added space around menu on smaller then 2xl

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import { MenuWrapper } from './menu-wrapper';
 
 export const Header = () => {
     return (
-        <header className="fixed max-w-screen-2xl w-full top-4 left-1/2 -translate-x-1/2 z-10">
+        <header className="fixed max-w-screen-2xl w-full top-4 left-1/2 -translate-x-1/2 z-10 px-4 2xl:px-0">
             <div className="bg-light border border-muted flex items-stretch rounded-full justify-between">
                 <div className="flex items-stretch flex-1">
                     <Link href="/">
